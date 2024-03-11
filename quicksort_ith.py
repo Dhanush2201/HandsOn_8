27f178c4bf4271f826a0a8537844846ed1ec3be9
Dhanush Chowdary <67065764+Dhanush2201@users.noreply.github.com>
2024-03-10 23:24:18 -0500

    Add files via upload

---
+++ quicksort_ith.py
@@ -0,0 +1,31 @@
+def partition(ar, l, h):
+    pivot = ar[h]
+    i = l - 1
+
+    for j in range(l, h):
+        if ar[j] < pivot:
+            i += 1
+            ar[i], ar[j] = ar[j], ar[i]
+
+    ar[i + 1], ar[h] = ar[h], ar[i + 1]
+    return i + 1
+
+def quickSelect(ar, l, h, i):
+    if l < h:
+        pi = partition(ar, l, h)
+
+        if pi == i:
+            return ar[pi]
+        elif pi < i:
+            return quickSelect(ar, pi + 1, h, i)
+        else:
+            return quickSelect(ar, l, pi - 1, i)
+
+    return ar[l]
+
+ar = [12, 5, 7, 3, 19, 26]
+n = len(ar)
+i = 2s
+
+result = quickSelect(ar, 0, n - 1, i)
+print(f"The {i}th order statistic is: {result}")
